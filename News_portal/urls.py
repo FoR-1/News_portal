48bed620507256887755e6e8af0def1a1d360044
FOR <toni.yudin2543@gmail.com>
2023-07-03 13:05:46 +0300

    3-commit

--- News_portal/urls.py
+++ News_portal/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # Импортируем созданные нами представления
-from .views import CategoryList, CategoryDetail
+from .views import CategoryList, CategoryDetail, PostList
 
 urlpatterns = [
    # path — означает путь.
@@ -9,6 +9,7 @@ urlpatterns = [
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
    path('', CategoryList.as_view()),
+   path('', PostList.as_view()),
    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
    # int — указывает на то, что принимаются только целочисленные значения
    path('<int:pk>', CategoryDetail.as_view()),
--- News_portal/views.py
+++ News_portal/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.views.generic import ListView, DetailView
-from .models import Category
+from .models import Category, Post
 
 
 class CategoryList(ListView):
@@ -18,4 +18,22 @@ class CategoryList(ListView):
 class CategoryDetail(DetailView):
     model = Category
     template_name = 'news.html'
-    context_object_name = 'category'
+    context_object_name = 'categorys'
+    
+
+class PostList(ListView):
+    model = Post
+    ordering = 'name'
+    template_name = 'post.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'posts'
